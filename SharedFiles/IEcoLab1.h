e7dc6538c51797083af465fddc6cadf9c40d7995
SkyPlusik <150513344+SkyPlusik@users.noreply.github.com>
2025-10-05 20:45:15 +0300

    Update IEcoLab1.h

--- SharedFiles/IEcoLab1.h
+++ SharedFiles/IEcoLab1.h
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * <кодировка символов>
  *   Cyrillic (UTF-8 with signature) - Codepage 65001
  * </кодировка символов>
@@ -31,26 +31,6 @@
 static const UGUID IID_IEcoLab1 = {0x01, 0x10, {0x27, 0x7F, 0xC0, 0x0C, 0x35, 0x62, 0x40, 0x96, 0xAF, 0xCF, 0xC1, 0x25, 0xB9, 0x4E, 0xEC, 0x90}};
 #endif /* __IID_IEcoLab1 */
 
-/* Структура для комплексных чисел */
-typedef struct EcoComplex {
-    double real;
-    double imag;
-} EcoComplex;
-
-/* Структура для вектора */
-typedef struct {
-    void *data;        
-    size_t size; 
-	char type; // 'i' - int, 'l' - long, 'f' - float, 'd' - double, 'F' - long double, 'c' - complex
-} EcoVector;
-
-/* Структура для матрицы */
-typedef struct {
-    double** data;
-    int rows;
-    int cols;
-} EcoMatrix;
-
 typedef struct IEcoLab1* IEcoLab1Ptr_t;
 
 typedef struct IEcoLab1VTbl {
@@ -61,14 +41,16 @@ typedef struct IEcoLab1VTbl {
     uint32_t (ECOCALLMETHOD *Release)(/* in */ IEcoLab1Ptr_t me);
 
     /* IEcoLab1 */
-    int16_t (ECOCALLMETHOD *polyHornerInt)(/* in */ IEcoLab1Ptr_t me, /* in */ int* roots, /* in */ uint32_t numRoots, /* out */ int** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerLong)(/* in */ IEcoLab1Ptr_t me, /* in */ int64_t* roots, /* in */ uint32_t numRoots, /* out */ int64_t** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerFloat)(/* in */ IEcoLab1Ptr_t me, /* in */ float* roots, /* in */ uint32_t numRoots, /* out */ float** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerDouble)(/* in */ IEcoLab1Ptr_t me, /* in */ double* roots, /* in */ uint32_t numRoots, /* out */ double** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerLongDouble)(/* in */ IEcoLab1Ptr_t me, /* in */ long double* roots, /* in */ uint32_t numRoots, /* out */ long double** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerComplex)(/* in */ IEcoLab1Ptr_t me, /* in */ EcoComplex* roots, /* in */ uint32_t numRoots, /* out */ EcoComplex** coefficients, /* out */ uint32_t* numCoefficients);
-    int16_t (ECOCALLMETHOD *polyHornerVector)(/* in */ IEcoLab1Ptr_t me, /* in */ EcoVector* roots, /* in */ uint32_t numRoots, /* out */ EcoVector** coefficients, /* out */ uint32_t* numCoefficients);
-	int16_t (ECOCALLMETHOD *polyHornerMatrix)(/* in */ IEcoLab1Ptr_t me, /* in */ EcoMatrix* mat, /* out */ EcoVector** coefficients, /* out */ uint32_t* numCoefficients);
+	int16_t (ECOCALLMETHOD *bucketSortIntWithK)(/* in */ IEcoLab1Ptr_t me, /* in */ int* arr, /* in */ uint32_t size, /* in */ uint32_t k, /* out */ int** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortIntAutoK)(/* in */ IEcoLab1Ptr_t me, /* in */ int* arr, /* in */ uint32_t size, /* out */ int** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortLongWithK)(/* in */ IEcoLab1Ptr_t me, /* in */ int64_t* arr, /* in */ uint32_t size, /* in */ uint32_t k, /* out */ int64_t** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortLongAutoK)(/* in */ IEcoLab1Ptr_t me, /* in */ int64_t* arr, /* in */ uint32_t size, /* out */ int64_t** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortFloatWithK)(/* in */ IEcoLab1Ptr_t me, /* in */ float* arr, /* in */ uint32_t size, /* in */ uint32_t k, /* out */ float** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortFloatAutoK)(/* in */ IEcoLab1Ptr_t me, /* in */ float* arr, /* in */ uint32_t size, /* out */ float** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortDoubleWithK)(/* in */ IEcoLab1Ptr_t me, /* in */ double* arr, /* in */ uint32_t size, /* in */ uint32_t k, /* out */ double** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortDoubleAutoK)(/* in */ IEcoLab1Ptr_t me, /* in */ double* arr, /* in */ uint32_t size, /* out */ double** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortLongDoubleWithK)(/* in */ IEcoLab1Ptr_t me, /* in */ long double* arr, /* in */ uint32_t size, /* in */ uint32_t k, /* out */ long double** sortedArr);
+	int16_t (ECOCALLMETHOD *bucketSortLongDoubleAutoK)(/* in */ IEcoLab1Ptr_t me, /* in */ long double* arr, /* in */ uint32_t size, /* out */ long double** sortedArr);
 
 } IEcoLab1VTbl, *IEcoLab1VTblPtr;
 
